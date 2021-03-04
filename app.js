61ad2a778ae91cfc0a6fb3a02a272cb09e69238f
natallia-js <fedartsova@tut.by>
2021-03-04 19:23:33 +0300

    edit

--- app.js
+++ app.js
@@ -26,13 +26,13 @@ app.use('/api/data', require('./routes/data.routes'));
 if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
-  app.get('*', (req, res) => {
+  app.get('*', (_req_, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
 
 // Порт сервера
-const PORT = config.get('port') || process.env.PORT || 4005;
+const PORT = process.env.PORT || config.get('port') || 4005;
 
 /**
  * Действия, которые необходимо выполнить для запуска сервера.
@@ -40,7 +40,7 @@ const PORT = config.get('port') || process.env.PORT || 4005;
 async function start() {
   try {
     // Запускаем http-сервер на указанном порту
-    app.listen(PORT, '0.0.0.0', () => console.log(`Server started on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (e) {
     console.log('Server Error', e.message);
     process.exit(1);
