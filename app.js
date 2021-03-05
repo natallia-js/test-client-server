0f0f1160c6b03bfac66a98fb3e841113548f62d7
natallia-js <fedartsova@tut.by>
2021-03-05 15:10:39 +0300

    edit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const config = require('config');
+const mongoose = require('mongoose');
 
 // Создаем объект приложения express
 const app = express();
